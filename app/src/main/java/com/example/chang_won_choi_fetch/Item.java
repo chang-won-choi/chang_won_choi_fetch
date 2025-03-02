f629938c73cdc6a3aeca80ef7db8beef326b8024
Chang Won Choi <cchang0705@gmail.com>
2025-03-01 22:58:19 -0600

    Created a data model for the list items.

---
+++ app/src/main/java/com/example/chang_won_choi_fetch/Item.java
@@ -0,0 +1,26 @@
+package com.example.chang_won_choi_fetch;
+
+public class Item {
+    private int id;
+    private int listId;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getListId() {
+        return id;
+    }
+    public void setListId(int listId) {
+        this.listId = listId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
