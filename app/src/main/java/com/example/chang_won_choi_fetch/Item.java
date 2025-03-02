02c6dca9626efe63223ffcd629a4006f8e8dff50
Chang Won Choi <cchang0705@gmail.com>
2025-03-01 23:07:53 -0600

    Fixed listId getter function and added a comment.

--- app/src/main/java/com/example/chang_won_choi_fetch/Item.java
+++ app/src/main/java/com/example/chang_won_choi_fetch/Item.java
@@ -1,5 +1,8 @@
 package com.example.chang_won_choi_fetch;
 
+/***
+ * A Class for list items retrieved from the API.
+ ***/
 public class Item {
     private int id;
     private int listId;
@@ -12,7 +15,7 @@ public class Item {
         this.id = id;
     }
     public int getListId() {
-        return id;
+        return listId;
     }
     public void setListId(int listId) {
         this.listId = listId;
